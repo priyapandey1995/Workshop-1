e1bba8c4b1f3059768090df3618ebbcf2270c7c2
priya <priyapandey8884.blr@gmail.com>
2022-08-05 00:29:59 +0530

    MODIFIED:[Priya] Player Symbol as X Or O for Playing Game

--- src/com/Bridgelabz/TicTacToe.java
+++ src/com/Bridgelabz/TicTacToe.java
@@ -1,9 +1,14 @@
 package com.Bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
 	private static char[] board = new char[10];
 	
+	static private char player1;
+	
+	static Scanner sc = new Scanner(System.in);
 	/**
 	 *  UC-1 - This method is Assigning space to Board Array
 	 */
@@ -13,12 +18,29 @@ public class TicTacToe {
 			}
 		
 	}
+	/**
+	 * UC-2 - Taking Playing symbol from the user either X or O
+	 */
+	
+	static void playerInput() {
+		System.out.println("Please Select the Symbol (X/0 :");
+		player1 = sc.next().charAt(0);
+		if(player1 == 'X' || player1 == 'O') {
+			System.out.println("Player 1 Symbol :"+player1);
+		}
+		else {
+			System.out.println("Invalid Symbol: Please Enter the valid symbol");
+			playerInput();
+		}
+		
+	}
 	
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Simulation");
 		
 		startGame();
+		playerInput();
 		
 	}
 
